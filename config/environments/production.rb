df2b2bc01caa11df2a7cfcbf9946a015ce6aa06b
zakairobbins <zakairobbins@gmail.com>
2016-01-19 17:30:08 -0800

    remove relative root url from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.relative_url_root = "/resumesthatresonate/public"
+  # config.relative_url_root = "/resumesthatresonate/public"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -35,7 +35,7 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.assets.prefix = "/"
+  # config.assets.prefix = "/"
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
